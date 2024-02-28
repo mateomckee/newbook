ddbe239db6be13ad907c91b367ee380f69009bc5
Nick Soto <nvsoto4@gmail.com>
2024-02-28 17:26:21 -0600

    added about us page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, PreloadingStrategy, RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
 const websiteName = 'Bluebook 2.0';
 
 const routes: Routes = [
   { path: '', component: HomeComponent},
+  { path: 'about-us', pathMatch: 'full', component: AboutUsComponent},
   { path: '**', pathMatch: 'full', component: PageNotFoundComponent, title: `${websiteName} - 404`}
 ];
 
