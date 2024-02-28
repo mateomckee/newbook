c55627acde64b33210acb6d4060f08c7cbfe1942
MikoBiko0609 <mad3566@icloud.com>
2024-02-28 17:23:54 -0600

    Results Page Added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, PreloadingStrategy, RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ResultsComponent } from './results/results.component';
 
 const websiteName = 'Bluebook 2.0';
 
 const routes: Routes = [
   { path: '', component: HomeComponent},
+  { path: 'results', pathMatch: 'full', component: ResultsComponent, title: `${websiteName} - Results`},
   { path: '**', pathMatch: 'full', component: PageNotFoundComponent, title: `${websiteName} - 404`}
 ];
 
