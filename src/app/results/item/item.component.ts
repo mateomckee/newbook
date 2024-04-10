72e3b342cde589a55a25bb241b1ab033cc915a3c
MikoBiko0609 <mad3566@icloud.com>
2024-04-10 01:59:27 -0500

    Image displacement, Hide teacher name if screen is too small, Separate teacher from course, Fix button icon positions, Header elements spacing, from edge of screen Search bar X padding

--- src/app/results/item/item.component.ts
+++ src/app/results/item/item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, HostListener } from '@angular/core';
 import { ItemData } from 'src/app/interfaces/item.interface';
 
 @Component({
@@ -9,4 +9,20 @@ import { ItemData } from 'src/app/interfaces/item.interface';
 export class ItemComponent {
   @Input() data = {} as ItemData;
   @Input() isSelected: boolean = false;
+  showInstructor: boolean = true;
+
+  constructor() {
+    // Call checkScreenSize when the component is created to set the initial state
+    this.checkScreenSize(window.innerWidth);
+  }
+
+  @HostListener('window:resize')
+  onResize() {
+    // Call checkScreenSize when the window is resized
+    this.checkScreenSize(window.innerWidth);
+  }
+
+  private checkScreenSize(width: number) {
+    this.showInstructor = (width > 500 && width <= 768) || width > 950;
+  }
 }
