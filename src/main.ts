e6585bb1d77d86141e8e565188343fa43cfc9419
mateomckee <mateomckee64@gmail.com>
2024-04-11 12:52:32 -0500

    Added Vercel Analytics

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app/app.module';
+import { inject } from '@vercel/analytics';
 
+inject();
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
